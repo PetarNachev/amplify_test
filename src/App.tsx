e30d1a68e19bb64f8f9dc24836c0b9895be8127d
PetarNachev <p.nachev92@gmail.com>
2024-07-13 17:58:39 +0300

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
 const client = generateClient<Schema>();
 
 function App() {
@@ -15,16 +18,21 @@ function App() {
 
   function createTodo() {
     let toDoContent = window.prompt("Todo content")
-    if (toDoContent){
-      client.models.Todo.create({ content: toDoContent});
-    } else{
+    if (toDoContent) {
+      client.models.Todo.create({ content: toDoContent });
+    } else {
       alert('Please enter a to do')
     }
-    
+
   }
 
   function updateTodo(id: string, content: string) {
-    client.models.Todo.update({ id, content: window.prompt("Edit to do", content) })
+    let todoUpdate = window.prompt("Edit to do", content)
+    if (todoUpdate) {
+      client.models.Todo.update({ id, content: todoUpdate })
+    } else {
+      alert('Please enter a to do')
+    }
   }
 
   function deleteTodo(id: string) {
@@ -32,24 +40,25 @@ function App() {
   }
 
   return (
-    <main>
-      <h1>To do list</h1>
-      <button onClick={createTodo}>+ add To do</button>
-      <ul>
-        {todos.map((todo) => (
-          <li
-            key={todo.id}>{todo.content}
-            <button onClick={() => deleteTodo(todo.id)}> Delete </button>
-            <button onClick={() => updateTodo(todo.id, todo.content!)}> Edit </button>
-          </li>
-        ))}
-      </ul>
-      <div>
 
-        <br />
-
-      </div>
-    </main>
+    <Authenticator>
+      {({ signOut }) => (
+        <main>
+          <h1>To do list</h1>
+          <button onClick={createTodo}>+ add To do</button>
+          <ul>
+            {todos.map((todo) => (
+              <li
+                key={todo.id}>{todo.content}
+                <button onClick={() => deleteTodo(todo.id)}> Delete </button>
+                <button onClick={() => updateTodo(todo.id, todo.content!)}> Edit </button>
+              </li>
+            ))}
+          </ul>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
